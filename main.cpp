f07c0c6a98c1e50ea16116892e36b47eb122a425
Vaishnavi Sawant <vvaishsawant@gmail.com>
2024-09-02 18:42:49 -0600

    Version - vaish801 : 1

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+std::vector<bool> LessMask(std::vector<double> nums, double less_than);
+std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
+
+int main(){
+    return 1;
+}
